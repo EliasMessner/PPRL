3893baa8774f42875e9715e0efbbedf7e3268067
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-01-04 13:46:28 +0100

    allow different order in parameters csv file

--- src/ArgumentHelper.java
+++ src/ArgumentHelper.java
@@ -11,9 +11,9 @@ public class ArgumentHelper {
     public static boolean parseBoolean(String[] args, String argName, boolean defaultValue) {
         String parsedValue = Arrays.stream(args).filter(a -> a.startsWith(argName + "="))
                 .findFirst().orElse(Boolean.toString(defaultValue));
-        if (parsedValue.endsWith("true")) {
+        if (parsedValue.toLowerCase().endsWith("true")) {
             return true;
-        } else if (parsedValue.endsWith("false")) {
+        } else if (parsedValue.toLowerCase().endsWith("false")) {
             return false;
         } else {
             throw new IllegalArgumentException("Cannot read boolean argument value '" + parsedValue + "'");
@@ -21,21 +21,21 @@ public class ArgumentHelper {
     }
 
     public static Parameters parseParametersFromArguments(String[] args) {
-        double t = Double.parseDouble(ArgumentHelper.parseString(args, "t", null));
-        int l = Integer.parseInt(ArgumentHelper.parseString(args, "l", null));
-        int k = Integer.parseInt(ArgumentHelper.parseString(args, "k", "10"));
         HashingMode hashingMode = HashingMode.parseFromString(ArgumentHelper.parseString(args, "mode", "DH"));
         boolean blocking = ArgumentHelper.parseBoolean(args, "b", false) ||
                 ArgumentHelper.parseBoolean(args, "blocking", false);
         boolean weightedAttributes = ArgumentHelper.parseBoolean(args, "wa", false) ||
                 ArgumentHelper.parseBoolean(args, "weightedAttributes", false);
-        return new Parameters(t, l, k, hashingMode, blocking, weightedAttributes);
+        int l = Integer.parseInt(ArgumentHelper.parseString(args, "l", null));
+        int k = Integer.parseInt(ArgumentHelper.parseString(args, "k", "10"));
+        double t = Double.parseDouble(ArgumentHelper.parseString(args, "t", null));
+        return new Parameters(hashingMode, blocking, weightedAttributes, l, k, t);
     }
 
     public static String parseString(String[] args, String argName, String defaultValue) {
         String parsedValue = Arrays.stream(args).filter(a -> a.startsWith(argName + "="))
                 .findFirst().orElse(defaultValue);
-        return parsedValue.substring(parsedValue.indexOf("=")+1);
+        return parsedValue.toLowerCase().substring(parsedValue.indexOf("=")+1);
     }
 
 }
--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,29 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class FileHandler {
 
     public static List<Parameters> parseParametersListFromFile(String filePath) throws IOException {
+        Map<String, Integer> attributeIndices;
         List<Parameters> parametersList = new ArrayList<>();
-        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-            stream.forEach(line -> parametersList.add(parseParametersFromLine(line)));
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+            attributeIndices = handleFirstLine(line);
+            while ((line = br.readLine()) != null) {
+                parametersList.add(parseParametersFromLine(line, attributeIndices));
+            }
         }
         return parametersList;
     }
 
-    private static Parameters parseParametersFromLine(String line) {
-        String[] args = line.trim().split(" ");
+    private static Parameters parseParametersFromLine(String line, Map<String, Integer> attributeIndices) {
+        String[] args = line.trim().split("[, ]");
         assert args.length == 6;
         return new Parameters(
-                Double.parseDouble(args[0]),
-                Integer.parseInt(args[1]),
-                Integer.parseInt(args[2]),
-                HashingMode.parseFromString(args[3]),
-                Boolean.parseBoolean(args[4]),
-                Boolean.parseBoolean(args[5]));
+                HashingMode.parseFromString(args[attributeIndices.get("mode")]),
+                Boolean.parseBoolean(args[attributeIndices.get("b")]),
+                Boolean.parseBoolean(args[attributeIndices.get("wa")]),
+                Integer.parseInt(args[attributeIndices.get("l")]),
+                Integer.parseInt(args[attributeIndices.get("k")]),
+                Double.parseDouble(args[attributeIndices.get("t")]));
+    }
+
+    private static Map<String, Integer> handleFirstLine(String line) {
+        Map<String, Integer> attributeIndices = new HashMap<>();
+        String[] args = line.trim().split("[, ]");
+        assert args.length == 6;
+        for (int i = 0; i < args.length; i++) {
+            attributeIndices.put(args[i], i);
+        }
+        return attributeIndices;
     }
 
 }
